decb6758d70caad667cb183c8c62796fd8354a47
Mathias Karstädt <mathias.karstaedt@gmail.com>
2024-01-10 22:29:36 +0100

    initial project setup

---
+++ Gemfile
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+ruby '3.0.6'
+
+group :test do
+  gem 'minitest'
+end
---
+++ test/sprite_fusion/tilemap_test.rb
@@ -0,0 +1,9 @@
+require 'minitest/autorun'
+
+class TilemapTest < Minitest::Test
+  def setup; end
+
+  def test_tilemap
+    assert false
+  end
+end
